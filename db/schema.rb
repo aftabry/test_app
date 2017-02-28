f89c69a100cd89408641730088f055f48edd9743
aftab afzal <aftabafzal58@gmail.com>
2017-02-28 12:59:01 +0500

    95. One to Many Association

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228065632) do
+ActiveRecord::Schema.define(version: 20170228075450) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "tittle"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
